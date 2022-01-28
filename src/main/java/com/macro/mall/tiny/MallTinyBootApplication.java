894ada85f20e9e9f058d00b23810e93e848b0caf
liangjing <1097281717@qq.com>
2022-01-28 10:55:03 +0800

    Initial commit

---
+++ src/main/java/com/macro/mall/tiny/MallTinyBootApplication.java
@@ -0,0 +1,16 @@
+package com.macro.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序启动类
+ */
+@SpringBootApplication
+public class MallTinyBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macro/mall/tiny/MallTinyBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.macro.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 基本的集成测试类
+ */
+@SpringBootTest
+class MallTinyBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
